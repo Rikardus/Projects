5a41d406f4a14fc399a3118da066775cc1e5b102
Ricardo Alcantara <83420903+Rikardus@users.noreply.github.com>
2024-01-20 20:32:50 -0300

    Update retirementCalculator.c

--- Input, Processing and Output/retirementCalculator.c
+++ Input, Processing and Output/retirementCalculator.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	int age, retire_age, years_left, date_retirement;
+	int age, retire_age, years_left, date_retirement, date;
 
 	time_t currentTime;
 	struct tm *localTime;
@@ -18,9 +18,11 @@ int main()
 	scanf("%d", &retire_age);
 
 	years_left = retire_age - age;
-
+	date = localTime->tm_year + 1900;
+	date_retirement = (localTime->tm_year + 1900) + years_left;
+	
 	printf("\nYou have %d years left until you can retire.\n", years_left);
-	printf("It's %04d, so you can retire in %04d\n", localTime->tm_year + 1900, (localTime->tm_year + 1900) + years_left);
+	printf("It's %04d, so you can retire in %04d\n", date, date_retirement);
 
 	return 0;
 }
